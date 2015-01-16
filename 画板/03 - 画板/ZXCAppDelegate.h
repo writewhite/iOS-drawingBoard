22b39a118d8db08f3371e1dbcd2a2ffb861f466b
Mac Z <zhanxuchang@MyAir.local>
2015-01-17 00:40:26 +0800

    添加更多数据

---
+++ 画板/03 - 画板/ZXCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZXCAppDelegate.h
+//  03 - 画板
+//
+//  Created by Mac Z on 14/11/6.
+//  Copyright (c) 2014年 Mac Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
